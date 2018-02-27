0ad3404f8e3f7106b12180cbc4b6ede205757109
kozistr <kozistr@gmail.com>
2018-02-28 07:11:51 +0900

    add ...

--- utils.py
+++ utils.py
@@ -2,13 +2,15 @@ from PIL import Image, ImageOps
 from urllib.request import urlretrieve
 
 import numpy as np
+import scipy.misc
 import os
 
 
 vgg19_download_link = 'http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat'
+vgg19_file_name = 'imagenet-vgg-verydepp-19.mat'
 
 
-def vgg19_download(file_name, expected_bytes=534904783):
+def vgg19_download(file_name=vgg19_file_name, expected_bytes=534904783):
     """ Download the pre-trained VGG-19 model if it's not already downloaded """
 
     if os.path.exists(file_name):
@@ -17,7 +19,7 @@ def vgg19_download(file_name, expected_bytes=534904783):
 
     print("[*] Downloading the VGG-19 pre-trained model...")
 
-    file_name, _ = urlretrieve(vgg19_download_link, file_name)
+    file_name, _ = urlretrieve(vgg19_download_link, vgg19_file_name)
     file_stat = os.stat(file_name)
 
     if file_stat.st_size == expected_bytes:
@@ -48,11 +50,29 @@ def image_resize(img_path, width, height, save=True):
     return np.expand_dims(image, axis=0)
 
 
-def setup_dir():
-    """ Making required directory """
+def image_save(img, img_path):
+    img = np.clip(img[0], 0., 255.).astype('uint8')
 
-    os.mkdir('./content/')
-    os.mkdir('./checkpoints/')
-    os.mkdir('./style/')
-    os.mkdir('./outputs/')
-    os.mkdir('./graphs/')
+    scipy.misc.imsave(img_path, img)
+
+
+def generate_noise_image(content_image, width, height, noise_range=20., noise_ratio=.6):
+    img_size = (1, height, width, 3)
+    noise_image = np.random.uniform(-noise_range, noise_range, img_size).astype(np.float32)
+
+    return noise_image * noise_ratio + content_image * (1. - noise_ratio)
+
+
+def setup_dir():
+    """ Creating directories if there's not one already """
+
+    def make_dir(name):
+        try:
+            os.mkdir(name)
+        except OSError:
+            pass
+
+    dirs = ['content', 'checkpoints', 'style', 'outputs', 'graphs']
+
+    for dir_ in dirs:
+        make_dir(dir_)
