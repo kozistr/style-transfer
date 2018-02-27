f839c6a23cf54a246ad2c1218f3a2f68ada0c59e
kozistr <kozistr@gmail.com>
2018-02-28 07:22:35 +0900

    add ...

--- vgg19.py
+++ vgg19.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 import scipy.io
 
+import utils
+
 
 def conv2d_layer(input_, weights, bias):
     """ convolution 2d layer with bias """
@@ -21,7 +23,20 @@ def pool2d_layer(input_, pool='avg'):
     return x
 
 
-class VGG19:
+class VGG19(object):
 
     def __init__(self):
-        pass
+        utils.vgg19_download()  # download vgg19 pre-trained model
+
+        self.vgg19_layers = (
+            'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
+            'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
+            'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
+            'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
+            'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
+            'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
+            'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
+            'relu5_3', 'conv5_4', 'relu5_4'
+        )
+
+        self.mean_pixels = np.array([123.68, 116.779, 103.939])
